b1e370f48f491f28deca7dde89b56daf811dee34
Gyozo Nagy <ngyozo@gmail.com>
2015-07-24 09:22:25 +0200

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://fccdbuser:3112fccdbuser3112@ds055742.mongolab.com:55742/fcc'
+  },
+
+  seedDB: true
+};
